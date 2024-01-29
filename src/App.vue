6b24af374d2f182ff9f108241812f1e51d58199e
hande-arch <156059093+hande-karatas@users.noreply.github.com>
2024-01-29 12:43:49 +0300

    class

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,14 @@ to define variables, methods and imports of other Vue compoennts. -->
 // in template. It is sort of templatecement of getElementById (but better)
 import { ref } from "vue";
 
-// Define variables and constants
-var count = ref(0);
+// Define variables and constants ref makes it reactive variable
+let count = ref(0);
+let name = ref("Hande")
 
 // Define functions
 function increment() {
   count.value++;
+  console.log(name)
 }
 </script>
 
@@ -21,7 +23,7 @@ with data, objects, functions etc. -->
   <div id="top-bar">
     <div id="title-container">
       <img class="logo-image" alt="Iaac logo" src="./assets/iaac-white.png" />
-      <h2>Digital Tools for Cloud-based Data Management</h2>
+      <h2>Digital Tools for Cloud-based Data Management {{ name }}</h2>
     </div>
   </div>
 
